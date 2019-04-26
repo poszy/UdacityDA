d40dcf9ea20fde9f6f34b6f58ee30dc82c261db9
Luis Pena <pena.m.luis@gmail.com>
2019-04-25 19:25:33 -0700

    Added Functions.py

--- Week 1/Lesson 2 Python Refresher/Functions.py
+++ Week 1/Lesson 2 Python Refresher/Functions.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python
+
+#Python functionsinvolves a function declaration def that takes one or more values,
+# and a return that returns one or more values. no data type required
+
+# If the function does not hae a return value or statement
+# the fucntion will return None
+
+# A pyhon GENERATOR is like a function, but instead of returning a values
+# The GENERATOR uses a YIELD statement . The YIELD statement pauses the process
+# 
