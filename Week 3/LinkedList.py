e75a3c30846005a1b9dc261c5e8c547f92af4d1b
poszy <pena.m.luis@gmail.com>
2019-05-16 22:55:00 -0700

    Added a few exercises to LinkedList

--- Week 3/LinkedList.py
+++ Week 3/LinkedList.py
@@ -18,6 +18,71 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next  = None
+# Create two new node obj with value = 2,1
+#head = Node(2)
+#new_node = Node(1)
+
+# assign next value of head obj equal to new node (1) 
+#head.next = new_node
+## new_node will have its own "value" variable.
+#new_node.value = head.next.value because head.next has its own class obj assigned, and that obj has its own variable of value. 
+#print (head.value)
+# head.next.NEW_NODE.VALUE
+#print (head.next.value)
 
 head = Node(2)
-print (head.value)
+head.next = Node(1)
+head.next.next = Node(4)
+head.next.next.next = Node(3)
+head.next.next.next.next = Node(5)
+
+def print_linked_list(head):
+    current_node = head
+
+    while current_node is not None:
+        print(current_node.value)
+        current_node = current_node.next
+#print_linked_list(head)
+
+#print (head.value)
+#print (head.next.value)
+#print (head.next.next.value)
+#print (head.next.next.next.value)
+#print (head.next.next.next.next.value)
+
+## Create a linked list based off of iteration/traversing
+
+def create_linked_list(input_list):
+    head = None
+    return head
+
+### Test Code
+def test_function(input_list, head):
+    try:
+        if len(input_list) == 0:
+            if head is not None:
+                print("Fail")
+                return
+        for value in input_list:
+            if head.value != value:
+                print("Fail")
+                return
+            else:
+                head = head.next
+        print("Pass")
+    except Exception as e:
+        print("Fail: "  + e)
+        
+        
+
+#input_list = [1, 2, 3, 4, 5, 6]
+#head = create_linked_list(input_list)
+#test_function(input_list, head)
+
+#input_list = [1]
+#head = create_linked_list(input_list)
+#test_function(input_list, head)
+
+#input_list = []
+#head = create_linked_list(input_list)
+#test_function(input_list, head)
