f4ecfbfbeeab87d9d7c043d6bb13ea801109a376
poszy <pena.m.luis@gmail.com>
2019-05-19 00:10:35 -0700

    Refactored Task1.py

--- Project 1/Task1.py
+++ Project 1/Task1.py
@@ -19,54 +19,81 @@ Print a message:
 "There are <count> different telephone numbers in the records."
 """
 
-# I am not sure if this question wants me to,
-# 1) find unique records from lists indivudually, and append records.
-# 2) do a direct comparison between both lists, record by record and filter for unique records
-# So I added both lists and filtered through the records for unqiue elements.
-
-# Creating two empty lists to append unique records.
-combinedLists  = []
-finalCallsList = []
-finalTextsList = []
-
-def trimListToNumber(uniqueList, recordType):
-
+# This function will get both phone numbers from
+# outgoing/receiving calls and texts
+# Param 1: takes in a record type of either calls or texts
+def trimListToNumber(recordType):
+    
+  # This list will be appened the phone numbers
+  uniqueList = []
+  
+  # Iterators that will run through the indexes of the senders
+  # and recievers of callers or texts.
   i = 0
   x = 0
+
+  # loop through the given recordType of either calls or texts
   for item in range (len (recordType)):
 
+    # Add the outgoing, caller or texters number
     uniqueList.append(recordType[i][0])
+
+    # Add the receiving, call or text receiver number
     uniqueList.append(recordType[x][1])
+
+    # Increment to have the next iteration add the next index.
     i = i + 1
     x = x + 1
 
+  # Return all numbers in one list.
   return uniqueList
 
-
+# This function takes in two lists and combines them.
+# Param #1:will take in the calls list that has both outgoing and receving numbers
+# Param #2:will take in the texts list that has both outgoing and receving texts numbers
 def combineLists(trimedCallsList, trimedTextsList):
+    
+  # Combine both list
   finalList = trimedCallsList + trimedTextsList
+
+  # Return the list
   return finalList
 
-# This function will filter through the final combined list
-# This filters for unique phone numbers
-def filterNumbers(resultList,cList):
-  for e in resultList:
-    if e not in cList:
-      cList.append(e)
-  return cList
+# this function will filter the combinedLists
+# Param #1: Takes in a combinedLists refference variables and
+# will filter, by adding only unique numbers to a new list
+def filterNumbers(combinedList):
+    
+  # Create a final list to add only the unique numbers
+  finalList = []
+
+  # Filter through combinedList
+  for e in combinedList:
+      
+    # if a number does not already exists, add it to the finalList
+    # if it is, do nothing.
+    if e not in finalList:
+      finalList.append(e)
+      
+  # return the final list    
+  return finalList
+
+
 
 # Set vars equal to the results of trimming the lists down to numbers
-a = trimListToNumber(finalCallsList,calls)
-b = trimListToNumber(finalTextsList,texts)
+allCallNumbers = trimListToNumber(calls)
+allTextNumbers = trimListToNumber(texts)
 
 # Set var equal to the result of combining previous lists that were trimmed down to numbers
-c = combineLists(a,b)
-f = []
-# Total number in combined calls list is 10,426
-#print (len(a))
-# TotalNumber in combined text numbers is 18,144
-#print (len(b))
-#print (len(combineLists(a,b)))
+allCombinedNumbers = combineLists(allCallNumbers,allTextNumbers)
+
+# Set var equal to final filtered List
+masterList = filterNumbers(allCombinedNumbers)
+
 
 # Finally print the result of the combined list filtered for unique numbers
-print (filterNumbers(c,f))
+print(
+    "There are " 
+    + str(len(masterList))
+    + " different telephone numbers in the records."
+    )
