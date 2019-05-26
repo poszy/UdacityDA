2b6a684347439000dc8411a8ad44a63f598ec045
Luis Pena <pena.m.luis@gmail.com>
2019-05-25 17:05:12 -0700

    Final Task0.py Added

--- Project 1/Task0.py
+++ Project 1/Task0.py
@@ -19,22 +19,5 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-# Prints first record of texts.
-print ("First record of texts, "
-       + str(texts[0][0])[1:-1]
-       + " texts "
-       + str(texts[0][1])[1:-1]
-       + " at time "
-       + str(texts[0][2])[1:-1] )
-
-# Prints out the last record in calls. 
-print ("Last Record of calls, "
-       + str(calls[-1][0])[1:-1]
-       + " calls "
-       + str(calls[-1][1])[1:-1]
-       + " at time, "
-       + str(calls[-1][2])[1:-1]
-       + " lasting "
-       + str(calls[-1][3])[1:-1]
-       + " seconds" )
-
+print('First record of texts, {0} texts {1} at time {2}'.format(*texts[0]))
+print('Last record of calls, {0} calls {1} at time {2}, lasting {3} seconds'.format(*calls[-1]))
