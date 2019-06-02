5fdb8f31a5cfe4d27adb0bcacca8c27bb8c4a05a
Luis Pena <pena.m.luis@gmail.com>
2019-06-02 14:53:45 -0700

    Added Trees

---
+++ Week 8/Trees.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+
+class Node:
+  def __init__(self):
+    self.right = None
+    self.left = None
+    self.value = None
+
+
+
+node0 = Node()
+print(f"""
+value: {node0.value}
+left: {node0.left}
+right: {node0.right}
+""")
