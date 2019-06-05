d37a86cae87c0872685db563ae3e1048874caf73
Luis Pena <pena.m.luis@gmail.com>
2019-06-05 13:14:01 -0700

    Added Problem4

---
+++ Project 2/problem_4.py
@@ -0,0 +1,63 @@
+class Group(object):
+
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+    def __str__(self):
+      return str(self.users)
+
+    __repr__ = __str__
+
+# Create Groups. Name of group self.name
+parent_group = Group("parent")
+child_group = Group("child")
+sub_child_group = Group("subchild")
+
+# Adding Group
+parent_group.add_group(child_group)
+child_group.add_group(sub_child_group)
+
+
+# Add User
+sub_child_user = "sub_child_user"
+sub_child_group.add_user(sub_child_user)
+
+
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+
+    """
+
+    print ('True') if user in group.get_users() else print ('False')
+
+
+
+# True
+is_user_in_group(sub_child_user, sub_child_group)
+
+# False
+is_user_in_group(sub_child_user, parent_group)
+is_user_in_group(sub_child_user, child_group)
