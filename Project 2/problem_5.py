301e7ba400221760d8f89a80f3f2b5bffe0e92db
poszy <pena.m.luis@gmail.com>
2019-06-08 01:05:14 -0700

    Finished problem5

--- Project 2/problem_5.py
+++ Project 2/problem_5.py
@@ -3,13 +3,13 @@
 import hashlib
 import datetime
 
-class BlockChainNode:
+class MainBlock:
+  
   def __init__(self, data, previous_hash):
     self.timestamp = self.calc_time()
     self.data = data
     self.previous_hash = previous_hash # NEXT
     self.hash = self.calc_hash()
-    self.next = None
 
   def calc_hash(self):
     sha = hashlib.sha256()
@@ -18,68 +18,61 @@ class BlockChainNode:
     return sha.hexdigest()
 
   def calc_time(self):
-
-    #print  (datetime.datetime.now())
     return (datetime.datetime.now().time())
 
-class LinkedList:
+class BlockNode:
+  def __init__(self, MainBlock):
+    self.value = MainBlock
+    self.next = None
+
+    
+
+class LinkedListBlock:
 
   def __init__(self):
     self.head = None
+    self.tail = None
 
-  def prepend(self, data, previous_hash):
+  def prepend(self, data):
 
     if self.head is None:
-      self.head = BlockChainNode(data, previous_hash)
-      print (self.head.previous_hash)
-      return
+      createBlock = MainBlock(data, None)
+      self.head = BlockNode(createBlock)
+      self.tail = self.head
+      
 
     else:
-      new_head = BlockChainNode( data, previous_hash)
-      new_head.next = self.head
-      self.head = new_head
+      createBlock = MainBlock( data, self.tail.value.hash)
+      self.tail.next = BlockNode(createBlock)
+      self.tail = self.tail.next
 
 
-  def append(self, data, previous_hash):
-    if self.head is None:
 
-      self.head = BlockChainNode(data, previous_hash)
+BlockChainName = LinkedListBlock()
+BlockChainName.prepend("According to my navi-computer, the--")
+BlockChainName.prepend("Shut up! Just shut up you idiot!")
+BlockChainName.prepend("Sheriff, this is no time to panic.")
+BlockChainName.prepend("This is the perfect time to panic! I'm lost, Andy is gone, they're gonna move into their house in two days and it's all your fault!")
 
-      print(self.head.data)
-
-      return
-
-    node = self.head
-    while node.next:
-      node = node.next
-
-    node.next = BlockChainNode(data, previous_hash)
-
-
-  def to_list(self):
-    output = []
-
-    node = self.head
-    while node:
-      output.append(str(node.timestamp) + str(node.data) + str(node.previous_hash))
-      node = node.next
-
-    print (output)
-
-ll = LinkedList()
-
-a = "this is a hash"
-b = "sadsadsa"
-#b = ll.calc_hash("b")
-#c = ll.calc_hash("c")
-#d = ll.calc_hash("d")
-
-
-ll.prepend(" my string " , a)
-#ll.prepend("thissadsa",b,"b")
-ll.append(" this string " , b )
-#ll.append("thasdadsadsadadsadsadis",d,"d")
-
-print(ll.to_list())
-
-#assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
+BlockChainName.prepend("MY-- my fault?! If you hadn't pushed me out of the window in the first place--")
+BlockChainName.prepend("OHH yeah?! Well if YOU hadn't shown up inside your stupid little cardboard spaceship and taken away everything that was important to me--")
+BlockChainName.prepend("Don't talk to me about importance! Because of you, the security of this entire universe is in jeopardy!")
+BlockChainName.prepend("WHAT?! What are you talking about?! ")
+BlockChainName.prepend("Right now, poised at the edge of the galaxy, Emperor Zurg has been secretly building a weapon with the destructive capacity to annihilate an entire planet! I alone have information that reveals this weapon's only weakness. And you my friend, are responsible for delayin' my rendezous with Star Command! ")
+BlockChainName.prepend("YOU-ARE-A-TOOOOOYY!!! You aren't the real Buzz Lightyear, you're-- oh, you're an action figure! You are a child's play thing!")
+BlockChainName.prepend("You are a sad, strange little man. And you have my pity. Farewell.")
+BlockChainName.prepend("Oh, yeah?! Well, good riddance, ya looney! [Muttering to himself] Rendezous with Star Command!")
+#####
+BlockNode = BlockChainName.head
+BlockIndexCount = 1
+   
+while BlockNode:
+  blockNode = BlockNode.value
+  print (BlockIndexCount)
+  print (blockNode.previous_hash)
+  print (blockNode.hash)
+  print (blockNode.timestamp)
+  print (blockNode.data)
+  BlockNode  = BlockNode.next
+  BlockIndexCount = BlockIndexCount + 1
+    
