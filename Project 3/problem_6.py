9c9c4202bb419350fefb8d8b4004929fda9434fb
poszy <pena.m.luis@gmail.com>
2019-06-25 21:13:58 -0700

    Added Test Cases to Problem6

--- Project 3/problem_6.py
+++ Project 3/problem_6.py
@@ -24,4 +24,12 @@ import random
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
+l2 = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l2)
+
+l3 = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l3)
+
 print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print ("Pass" if ((0, 9) == get_min_max(l2)) else "Fail")
+print ("Pass" if ((0, 9) == get_min_max(l3)) else "Fail")
