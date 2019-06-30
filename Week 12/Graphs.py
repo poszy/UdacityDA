a8f019fca72f677c4af016a26f51d2df77c49a1b
poszy <pena.m.luis@gmail.com>
2019-06-30 13:37:47 -0700

    Finished Graphs.py

--- Week 12/Graphs.py
+++ Week 12/Graphs.py
@@ -103,3 +103,41 @@ class Graph(object):
         if(node1 in self.nodes and node2 in self.nodes):
             node1.remove_child(node2)
             node2.remove_child(node1)
+
+
+def dfs_search(root_node, search_value):
+    visited = []
+    stack = [root_node]
+
+    while len(stack) > 0:
+        current_node = stack.pop()
+        visited.append(current_node)
+
+        if current_node.value == search_value:
+            return current_node
+
+        for child in current_node.children:
+            if child not in visited:
+                stack.append(child)
+
+
+nodeG = GraphNode('G')
+nodeR = GraphNode('R')
+nodeA = GraphNode('A')
+nodeP = GraphNode('P')
+nodeH = GraphNode('H')
+nodeS = GraphNode('S')
+
+graph1 = Graph([nodeS,nodeH,nodeG,nodeP,nodeR,nodeA] ) 
+graph1.add_edge(nodeG,nodeR)
+graph1.add_edge(nodeA,nodeR)
+graph1.add_edge(nodeA,nodeG)
+graph1.add_edge(nodeR,nodeP)
+graph1.add_edge(nodeH,nodeG)
+graph1.add_edge(nodeH,nodeP)
+graph1.add_edge(nodeS,nodeR)
+
+
+assert nodeA == dfs_search(nodeS, 'A')
+assert nodeS == dfs_search(nodeP, 'S')
+assert nodeR == dfs_search(nodeH, 'R')
