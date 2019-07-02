d753e9411398dbf74832ac36166482bd5ebf2398
Luis Pena <pena.m.luis@gmail.com>
2019-07-02 09:28:47 -0700

    Fixed Project 3

--- Project 3/problem_6.py
+++ Project 3/problem_6.py
@@ -8,12 +8,21 @@ def get_min_max(ints):
     min_number = ints[0]
     max_number = ints[0]
 
-    for index in ints:
-        if ints[index] < min_number:
-            min_number = ints[index]
+    if len(ints) > 10:
+      print("List is too large")
+      return None
 
-        if ints[index] > max_number:
-            max_number = ints[index]
+    for index in ints:
+
+      if ints[index] < 0 :
+        print ("Enter a valid positive integer")
+        break
+
+      if ints[index] < min_number:
+        min_number = ints[index]
+
+      if ints[index] > max_number:
+        max_number = ints[index]
 
     return(min_number,max_number)
 
@@ -24,12 +33,12 @@ import random
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
-l2 = [i for i in range(0, 10)]  # a list containing 0 - 9
+l2 = [-1,0,3,4,5,-1,4,6,-9,9]  # a list containing 0 - 9
 random.shuffle(l2)
 
-l3 = [i for i in range(0, 10)]  # a list containing 0 - 9
+l3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]  # a list containing 0 - 9
 random.shuffle(l3)
 
-print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
-print ("Pass" if ((0, 9) == get_min_max(l2)) else "Fail")
-print ("Pass" if ((0, 9) == get_min_max(l3)) else "Fail")
+print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")  # shold print pass
+print ("Pass" if ((0, 9) == get_min_max(l2)) else "Fail") # Should print Fail
+print ("Pass" if ((0, 9) == get_min_max(l3)) else "Fail") # Should print Fail
